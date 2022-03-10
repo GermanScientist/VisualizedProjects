0c7857fb09a0dc9ce2949b78139c94b8bbd37438
GermanScientist <jack_hulspas@hotmail.com>
2022-03-10 12:52:28 +0100

    Added a simple grid in a new state machine program

---
+++ VisualizedProjects/FiniteStateMachine/StateMachineProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Raylib_cs;
+
+namespace VisualizedProjects.Visualizer {
+    namespace VisualizedProjects.FiniteStateMachine {
+        class StateMachineProgram : ProgramBase {
+            public StateMachineProgram() {
+            }
+
+            public override void Update() {
+                for (int i = 0; i < 10; i++) {
+                    for (int j = 0; j < 10; j++) {
+                        Raylib.DrawRectangle(i * 50, j * 50, 50, 50, Color.WHITE); //Draw the cell rectangle
+                        Raylib.DrawRectangleLines(i * 50, j * 50, 50, 50, Color.BLACK); //Draw the cell outline
+                    }
+                }
+            }
+        }
+    }
+}
--- VisualizedProjects/Program.cs
+++ VisualizedProjects/Program.cs
@@ -1,9 +1,14 @@
 ﻿namespace VisualizedProjects.Visualizer {
-    namespace VisualizedProjects.PathFinding {
-        class Program {
-            static void Main(string[] args) {
-                Visualizer visualizer = new Visualizer("Pathfinding", new Pathfindinder()); //Create a window that runs the pathfinding program
-                while (visualizer.Run()) {; } //Create an update loop to start running the application
+    namespace VisualizedProjects.FiniteStateMachine {
+        namespace VisualizedProjects.PathFinding {
+            class Program {
+                static void Main(string[] args) {
+                    //Visualizer visualizer = new Visualizer("Pathfinding", new Pathfindinder()); //Create a window that runs the pathfinding program
+                    //while (visualizer.Run()) {; } //Create an update loop to start running the application
+
+                    Visualizer visualizer = new Visualizer("State Machine", new StateMachineProgram()); //Create a window that runs the pathfinding program
+                    while (visualizer.Run()) {; } //Create an update loop to start running the application
+                }
             }
         }
     }
